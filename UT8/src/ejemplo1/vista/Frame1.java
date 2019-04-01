e9680ba677d3d5706389445dc407e8d7d12e5617
AlvaroSC <alsancer2@gmail.com>
2019-04-01 18:02:53 +0200

    ..

--- UT8/src/ejemplo1/vista/Frame1.java
+++ UT8/src/ejemplo1/vista/Frame1.java
@@ -3,6 +3,11 @@ package ejemplo1.vista;
 import javax.swing.JFrame;
 
 public class Frame1 extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8200738056828413109L;
+
 	// Constructor
 	public Frame1() {
 	}
