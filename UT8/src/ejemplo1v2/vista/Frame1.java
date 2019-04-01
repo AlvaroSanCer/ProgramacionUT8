4f1f4726c9b832546fcc6ccfb513278adb562233
AlvaroSC <alsancer2@gmail.com>
2019-04-01 18:47:39 +0200

    ..

--- UT8/src/ejemplo1v2/vista/Frame1.java
+++ UT8/src/ejemplo1v2/vista/Frame1.java
@@ -2,6 +2,8 @@ package ejemplo1v2.vista;
 
 
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class Frame1 extends JFrame {
@@ -14,6 +16,6 @@ public class Frame1 extends JFrame {
 		this.setSize(600, 300);
 		this.setTitle("Mi primera ventana");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		this.setBackground(Color.GREEN);
 	}
 }
