cd23b20852ee53d2735ae6c9895c296481cb2820
AlvaroSC <alsancer2@gmail.com>
2019-04-01 18:38:51 +0200

    .

--- UT8/src/ejemplo1v2/vista/Frame1.java
+++ UT8/src/ejemplo1v2/vista/Frame1.java
@@ -11,7 +11,7 @@ public class Frame1 extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public Frame1() {
-		this.setSize(2000, 300);
+		this.setSize(600, 300);
 		this.setTitle("Mi primera ventana");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
