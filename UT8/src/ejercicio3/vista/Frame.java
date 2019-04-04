7786e7d8f7b571b3b026d2989066b0e4424bc840
AlvaroSC <alsancer2@gmail.com>
2019-04-04 21:41:33 +0200

    :)

--- UT8/src/ejercicio3/vista/Frame.java
+++ UT8/src/ejercicio3/vista/Frame.java
@@ -3,8 +3,44 @@ package ejercicio3.vista;
 import java.awt.Container;
 
 import javax.swing.JFrame;
+import javax.swing.JToggleButton;
+import java.awt.BorderLayout;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
 
 public class Frame extends JFrame {
+	
+	
+	public Frame() {
+		getContentPane().setForeground(Color.BLACK);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setForeground(Color.BLACK);
+		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		comboBox.setBackground(Color.LIGHT_GRAY);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Rojo", "Verde", "Azul", "Morado"}));
+		getContentPane().add(comboBox, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Segundo plano");
+		chckbxNewCheckBox.setFont(new Font("Tahoma", Font.BOLD, 13));
+		chckbxNewCheckBox.setForeground(Color.BLUE);
+		chckbxNewCheckBox.setBounds(78, 24, 121, 23);
+		panel.add(chckbxNewCheckBox);
+		
+		JCheckBox chckbxPrimerPlano = new JCheckBox("Primer plano");
+		chckbxPrimerPlano.setForeground(Color.BLUE);
+		chckbxPrimerPlano.setFont(new Font("Tahoma", Font.BOLD, 13));
+		chckbxPrimerPlano.setBounds(227, 24, 134, 23);
+		panel.add(chckbxPrimerPlano);
+	}
 
 
 
@@ -12,7 +48,4 @@ public class Frame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = -4892418694164186384L;
-	
-	private Container gestor1,gestor2,gestor3;
-	
 }
