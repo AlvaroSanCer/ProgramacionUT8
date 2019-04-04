390bdc94a163cdb885d30bada47e83f94cdcecf3
AlvaroSC <alsancer2@gmail.com>
2019-04-04 20:57:23 +0200

    ejercicio8

--- UT8/src/ejercicio3/vista/Frame.java
+++ UT8/src/ejercicio3/vista/Frame.java
@@ -8,5 +8,11 @@ public class Frame extends JFrame {
 
 
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4892418694164186384L;
+	
 	private Container gestor1,gestor2,gestor3;
+	
 }
